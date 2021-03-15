fc960a8dd37fc0e557c8006cf7b1e1c19afc4da8
Diego <diego2000avelar@gmail.com>
2021-03-15 10:28:19 -0600

    removing credentials

--- config/nylas.config.js
+++ config/nylas.config.js
@@ -2,8 +2,8 @@
 const Nylas = require("nylas")
 
 Nylas.config({
-    clientId: "2tfto6xg8h5kce6loz54d4bwx",
-    clientSecret: "blzuh88vh7irnux5a3wyhn0zq"
+    clientId: "",
+    clientSecret: ""
 })
 
-exports.nylas = Nylas.with("jZU3BA526Rq6oC8FGb6D2zIKZJn6XL")
+exports.nylas = Nylas.with("")
